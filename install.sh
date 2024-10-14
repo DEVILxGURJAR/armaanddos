934f33cc50d27c5317841b978e4fa9773a3f602c
DEVILxGURJAR <devilgujjar678786@gmail.com>
2024-10-14 18:09:43 +0000

    Pending changes exported from your codespace

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/vampire/vampire.py",
+    "postStartCommand": "python3 /workspaces/armaanddos/vampire.py",
     "customizations": {
         "vscode": {
             "settings": {
